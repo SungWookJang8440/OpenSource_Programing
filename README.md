37eed01f617733903e0d1f3f315894bfc1e5bea6
SungWookJang8440 <csu8440@gmail.com>
2026-03-13 11:08:25 +0900

    feat : mini_project1 기능 구현

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # OpenSource_Programing
+
+## Mini_project1
+
+
+
+## 기능
+총 3개의 기능(페이지)으로 구성되어 있습니다.
+
+1. **`/` (Home)**: API 서버 접속 환영 메시지 출력
+2. **`/profile` (Profile)**: 개발자(장성욱 - 경북대학교 컴퓨터학부)의 기본 인적 사항 출력
+3. **`/skills` (Skills)**: 주력 개발 스택(Python) 출력
+
+## 🛠 실행 방법
+```bash
+# 1. 패키지 설치
+pip install -r requirements.txt
+
+# 2. 서버 실행
+python app.py
--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# 1. 홈 페이지
 @app.route("/")
-def index() :
+def index():
     return "Welcome to My Development Journal API"
 
+# 2. 프로필 페이지
+@app.route("/profile")
+def profile():
+    return "이름 : 장성욱<br>학력 : 경북대학교 컴퓨터학부"
+
+# 3. 스킬 페이지
+@app.route("/skills")
+def skills():
+    return "Python"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
